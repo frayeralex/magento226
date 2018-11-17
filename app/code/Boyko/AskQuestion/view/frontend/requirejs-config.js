a44ae642bbb834a625c7e6251ba8170c852f9a89
Oleksiy Boyko <frayeralex@gmail.com>
2018-11-17 14:05:25 +0200

    add submit controller add form-question handler widget add prevention many requests

--- app/code/Boyko/AskQuestion/view/frontend/requirejs-config.js
+++ app/code/Boyko/AskQuestion/view/frontend/requirejs-config.js
@@ -1,5 +1,6 @@
 var config = {
     paths: {
-        customUaPhoneValidator: 'Boyko_AskQuestion/js/ua-phone-number-validator'
+        customUaPhoneValidator: 'Boyko_AskQuestion/js/ua-phone-number-validator',
+        handleQuestionFormSubmit: 'Boyko_AskQuestion/js/handle-question-form-submit'
     }
 };
---
+++ app/code/Boyko/AskQuestion/view/frontend/web/js/handle-question-form-submit.js
@@ -0,0 +1,120 @@
+define([
+    'jquery',
+    'moment',
+    'Magento_Ui/js/modal/alert',
+    'mage/cookies',
+    'mage/translate',
+    'jquery/ui'
+], function ($, moment, alert) {
+    'use strict';
+
+    $.widget('boyko.askQuestionHandler', {
+        options: {
+            cookieName: 'boyko_question_was_requested'
+        },
+
+        /** @inheritdoc */
+        _create: function () {
+            $(this.element).submit(this.submitForm.bind(this));
+        },
+
+        /**
+         * Validate request and submit the form if able
+         */
+        submitForm: function () {
+            if (!this.validateForm()) {
+                alert('Form invalid');
+
+                return;
+            }
+
+            this.ajaxSubmit();
+        },
+
+        /**
+         * Submit request via AJAX. Add form key to the post data.
+         */
+        ajaxSubmit: function () {
+            if (!this.isSubmitTimeoutExceeded()) {
+                return alert({
+                    title: $.mage.__('Too many request'),
+                    content: $.mage.__('Please waite couple minutes for submit new one question')
+                });
+            }
+            var formData = new FormData($(this.element).get(0));
+
+            formData.append('form_key', $.mage.cookies.get('form_key'));
+            formData.append('isAjax', 1);
+
+            $.ajax({
+                url: $(this.element).attr('action'),
+                data: formData,
+                processData: false,
+                contentType: false,
+                type:  $(this.element).attr('method'),
+                dataType: 'json',
+                context: this,
+
+                /** @inheritdoc */
+                success: function (response) {
+                    $('body').trigger('processStop');
+                    alert({
+                        title: $.mage.__(response.status),
+                        content: $.mage.__(response.message)
+                    });
+
+                    if (response.status === 'Success') {
+                        // Prevent from sending requests too often
+                        $.mage.cookies.set(
+                            this.options.cookieName,
+                            Date.now()
+                        );
+                    }
+                },
+
+                /** @inheritdoc */
+                error: function () {
+                    $('body').trigger('processStop');
+                    alert({
+                        title: $.mage.__('Error'),
+                        /*eslint max-len: ["error", { "ignoreStrings": true }]*/
+                        content: $.mage.__('Your request can not be submitted right now. Please, contact us directly via email or phone to get your Sample.')
+                    });
+                }
+            });
+        },
+
+        getLastSuccessRequestMoment: function() {
+            var lastRequestTimestamp = $.mage.cookies
+                .get(this.options.cookieName);
+
+            return lastRequestTimestamp ? moment(+lastRequestTimestamp) : null;
+        },
+
+        isSubmitTimeoutExceeded: function() {
+            if (!this.getLastSuccessRequestMoment()) {
+                return true;
+            }
+
+            return moment().subtract(2, 'minute')
+                .isAfter(this.getLastSuccessRequestMoment());
+        },
+
+        /**
+         * Validate request form
+         */
+        validateForm: function () {
+            return $(this.element).validation().valid();
+        },
+
+        /**
+         * Clear that `geekhub_request_sample_clear_cookie` cookie
+         * when the event `geekhub_request_sample_clear_cookie` is triggered
+         */
+        clearCookie: function () {
+            $.mage.cookies.clear(this.options.cookieName);
+        }
+    });
+
+    return $.boyko.askQuestionHandler;
+});
