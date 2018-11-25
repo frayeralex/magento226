3179e25814def23112ec61a0e4d8f816d1dc4abf
Oleksiy Boyko <frayeralex@gmail.com>
2018-11-25 14:32:05 +0200

    fix review issues: - add validation for modal clone registration new user from - add prettier script for auto format app/**/*.js files - add pre-commit hook

--- app/code/Boyko/Customer/view/frontend/web/js/popup.js
+++ app/code/Boyko/Customer/view/frontend/web/js/popup.js
@@ -1,29 +1,52 @@
-define(['jquery', 'Magento_Ui/js/modal/modal'], function ($, modal) {
+define(['jquery', 'Magento_Ui/js/modal/modal'], function($, modal) {
     'use strict';
 
-    return function(config, element) {
-        const $btn = $(element);
-        const $cloneForm = $('.form-create-account').clone();
+    return function(config, element, data) {
+        var $btn = $(element),
+            prefix = data && data.prefix ? data.prefix : 'clone',
+            $cloneForm = $('.form-create-account').clone();
+        $cloneForm.attr('id', prefix + '-' + 'form-create-account');
 
-        const popup = modal({
-            type: 'popup',
-            responsive: true,
-            innerScroll: true,
-            title: $.mage.__('Registration for dealer'),
-            buttons: [
-                {
-                    text: $.mage.__('Close'),
-                    class: 'action primary',
-                    click: function () {
-                        this.closeModal();
-                    }
-                }
-            ]
-        }, $cloneForm);
+        $cloneForm.find('[id]').each(function(index, element) {
+            var $element = $(element),
+                id = $(element).attr('id'),
+                name = $element.attr('name');
 
-        $btn.on('click', function () {
-            popup.openModal();
+            $element.attr('id', prefix + '_' + id);
+            if (name) {
+                $element.attr('name', prefix + '_' + name);
+            }
         });
-    }
-});
+        $cloneForm.find('[for]').each(function(index, element) {
+            var $element = $(element),
+                htmlFor = $(element).attr('for');
 
+            $element.attr('for', prefix + htmlFor);
+        });
+
+        var popup = modal(
+            {
+                type: 'popup',
+                responsive: true,
+                innerScroll: true,
+                title: $.mage.__('Registration for dealer'),
+                buttons: [
+                    {
+                        text: $.mage.__('Close'),
+                        class: 'action primary',
+                        click: function() {
+                            this.closeModal();
+                        }
+                    }
+                ]
+            },
+            $cloneForm
+        );
+
+        $btn.on('click', function() {
+            var modal = popup.openModal();
+            $.initFormValidation($cloneForm);
+            modal.trigger('contentUpdated');
+        });
+    };
+});
